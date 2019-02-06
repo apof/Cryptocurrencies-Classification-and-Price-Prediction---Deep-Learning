b06965be33eec426070609326bf190a7c83e11cd
Apostolos Florakis <32836747+apof@users.noreply.github.com>
2019-02-07 00:11:30 +0200

    Add files via upload

--- Src/visualize_data.py
+++ Src/visualize_data.py
@@ -4,65 +4,109 @@ import matplotlib.pyplot as plt
 import matplotlib
 import csv
 import utils
+from sklearn import preprocessing
+import math
+from scipy import stats
+
 
 DATA_DIR = "../Datasets/all_vectors_merged.csv"
 ASSETS_DIR = "../Datasets/assets_number.csv"
 
-data = utils.load_dataset(DATA_DIR)
-assets_number = utils.load_dataset(ASSETS_DIR)
+def plot_features(data):
 
-number_per_asset = []
-for row in assets_number.iterrows():
-	for i in range(0,len(row[1])):
-		number_per_asset.append(row[1][i])
+	assets_number = utils.load_dataset(ASSETS_DIR)
 
-asset_frames = []
-start = 0
+	number_per_asset = []
+	for row in assets_number.iterrows():
+		for i in range(0,len(row[1])):
+			number_per_asset.append(row[1][i])
 
-for i in range(0,len(number_per_asset)):
-	end = start + number_per_asset[i]
-	frame = pd.DataFrame()
-	frame = data[start:end]
-	asset_frames.append(frame)
-	start = end
+	asset_frames = []
+	start = 0
 
-Fet_names = []
-Fet_names.append('Open')
-Fet_names.append('High')
-Fet_names.append('Low')
-Fet_names.append('Close')
-Fet_names.append('Volume')
-Fet_names.append('MarketCup')
-Fet_names.append('Forks')
-Fet_names.append('Stars')
-Fet_names.append('Subscribers')
-Fet_names.append('Total_issues')
-Fet_names.append('Closed_issues')
-Fet_names.append('Pull_requests_merged')
-Fet_names.append('Pull_request_contributors')
-Fet_names.append('Commit_count_4_weeks')
-Fet_names.append('Blockcount')
-Fet_names.append('Mediafee')
-Fet_names.append('Txcount')
-Fet_names.append('Activeaddresses')
+	for i in range(0,len(number_per_asset)):
+		end = start + number_per_asset[i]
+		frame = pd.DataFrame()
+		frame = data[start:end]
+		asset_frames.append(frame)
+		start = end
 
-Fets = []
-for i in range(0,len(Fet_names)):
-	Fets.append([])
+	Fet_names = []
+	Fet_names.append('Open')
+	Fet_names.append('High')
+	Fet_names.append('Low')
+	Fet_names.append('Close')
+	Fet_names.append('Volume')
+	Fet_names.append('MarketCup')
+	Fet_names.append('Forks')
+	Fet_names.append('Stars')
+	Fet_names.append('Subscribers')
+	Fet_names.append('Total_issues')
+	Fet_names.append('Closed_issues')
+	Fet_names.append('Pull_requests_merged')
+	Fet_names.append('Pull_request_contributors')
+	Fet_names.append('Commit_count_4_weeks')
+	Fet_names.append('Blockcount')
+	Fet_names.append('Mediafee')
+	Fet_names.append('Txcount')
+	Fet_names.append('Activeaddresses')
 
-Indexes = []
-index = 1
+	Fets = []
+	for i in range(0,len(Fet_names)):
+		Fets.append([])
 
-for row in asset_frames[1].iterrows():
-	for i in range(0,len(Fets)):
-		Fets[i].append(row[1][i+2])
-	Indexes.append(index)
-	index += 1
+	Indexes = []
+	index = 1
 
-i=0
-plt.plot(Indexes,Fets[i])
-plt.xlabel('data index')
-plt.ylabel(Fet_names[i])
-plt.savefig('Plots/'+Fet_names[i]+'_btc.png')
+	for row in asset_frames[1].iterrows():
+		for i in range(0,len(Fets)):
+			Fets[i].append(row[1][i+2])
+		Indexes.append(index)
+		index += 1
+
+	i=3
+	plt.plot(Indexes,Fets[i])
+	plt.xlabel('data index')
+	plt.ylabel(Fet_names[i])
+	plt.savefig('Plots_Results/'+Fet_names[i]+'_btc.png')
+
+def get_bitcoin_batch(data):
+
+	data.set_index("asset", inplace=True)
+	btc_frame = data.loc['btc']
+
+	#return all btc columns exept from date
+	return btc_frame.loc[:, btc_frame.columns != 'date']
 
 
+def data_scaling(df,scaling_method):
+	
+	#returns a numpy array
+	x = df.values
+
+	#check that standarization is correct
+	#transposed_df = df.transpose()
+	#xx = transposed_df.values
+	#print stats.zscore(xx[0])
+
+	if(scaling_method == 'standarization'):
+		res = pd.DataFrame(preprocessing.StandardScaler().fit_transform(x),columns=df.columns, index=df.index)
+	elif(scaling_method == 'normalization'):
+		res = pd.DataFrame(preprocessing.MinMaxScaler().fit_transform(x),columns=df.columns, index=df.index)
+
+	return res.transpose()
+
+
+def main():
+
+	data = utils.load_dataset(DATA_DIR)
+	#plot_features(data)
+
+	btc_data = get_bitcoin_batch(data)
+	btc_scaled_data = data_scaling(btc_data,'normalization')
+
+	print btc_scaled_data.transpose()
+
+
+if __name__ == "__main__":
+    main()
